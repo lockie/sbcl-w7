46e4c9b7236caab3665c425a5594f64d1296d3a8
Andrew Kravchuk <awkravchuk@gmail.com>
2023-05-04 17:30:35 +0200

    Fixed missing ffs(sic) function
    see https://gnu.org/software/gnulib/manual/html_node/ffs.html

--- build.sh
+++ build.sh
@@ -9,6 +9,7 @@ export CC=win-dupebin-cc
 
 cd ../sbcl
 sed -i 's/gcc/win-dupebin-cc/g' src/runtime/Config.x86-64-win32
+export CFLAGS='-Dffs=__builtin_ffs'
 ./make.sh --with-sb-core-compression --xc-host=../run_sbcl.sh --arch=x86_64 || true
 cp ../sbcl.nsi .
 makensis sbcl.nsi
