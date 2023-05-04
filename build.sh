c3fe0dd5e7734c4ac47c196fd18b992e5ff80d5e
Andrew Kravchuk <awkravchuk@gmail.com>
2023-05-04 17:50:34 +0200

    Even better fix for ffs
    ffs

--- build.sh
+++ build.sh
@@ -5,10 +5,11 @@ set -exo pipefail
 source env/all.src
 source env/duplicate_exe_outputs.src
 export PATH=`dirname $(pwd)`:$PATH:`pwd`/env/wrappers
-export CC='win-dupebin-cc -Dffs=__builtin_ffs'
+export CC=win-dupebin-cc
 
 cd ../sbcl
 sed -i 's/gcc/win-dupebin-cc/g' src/runtime/Config.x86-64-win32
+sed -i 's/extern int ffs(int);/#define ffs __builtin_ffs/g' src/runtime/hopscotch.c
 ./make.sh --with-sb-core-compression --xc-host=../run_sbcl.sh --arch=x86_64 || true
 cp ../sbcl.nsi .
 makensis sbcl.nsi
