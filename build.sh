aed91d110f76743b698d390ef868a66ef81cc20c
Andrew Kravchuk <awkravchuk@gmail.com>
2023-05-05 14:53:46 +0200

    Fixed failing clang build
    see https://bugs.launchpad.net/sbcl/+bug/2018601

--- build.sh
+++ build.sh
@@ -11,6 +11,8 @@ cd ../sbcl
 sed -i 's/gcc/win-dupebin-cc/g' src/runtime/Config.x86-64-win32
 sed -i 's/extern int ffs(int);/#define ffs __builtin_ffs/g' src/runtime/hopscotch.c
 sed -i 's/-WIP//g' generate-version.sh
+wget -q -O /tmp/clang.patch https://launchpadlibrarian.net/664725928/clang.patch
+patch -p1 < /tmp/clang.patch
 ./make.sh --with-sb-core-compression --xc-host=../run_sbcl.sh --arch=x86_64 || true
 cp ../sbcl.nsi .
 makensis sbcl.nsi
