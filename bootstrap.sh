4d138f4439b9ec0bae245f46c4ba6a878c8bcb60
Andrew Kravchuk <awkravchuk@gmail.com>
2023-05-15 16:34:35 +0200

    Fixed failures building contribs

--- bootstrap.sh
+++ bootstrap.sh
@@ -6,7 +6,7 @@ export TERM=linux
 export DEBIAN_FRONTEND="noninteractive"
 dpkg --add-architecture i386
 apt-get update -qq
-apt-get install -q -y --no-install-recommends ca-certificates git gpg make msitools nsis tar wget wine winetricks xz-utils zstd
+apt-get install -q -y --no-install-recommends ca-certificates git gpg make msitools nsis tar unzip wget wine winetricks xz-utils zstd
 
 winetricks win7
 wget -q -O /tmp/sbcl-1.4.14-x86-64-windows-binary.msi "https://sourceforge.net/projects/sbcl/files/sbcl/1.4.14/sbcl-1.4.14-x86-64-windows-binary.msi/download"
@@ -18,5 +18,8 @@ git clone --branch sbcl-${SBCL_VERSION} git://git.code.sf.net/p/sbcl/sbcl
 git clone --depth=1 https://github.com/HolyBlackCat/quasi-msys2
 cd quasi-msys2
 make install _gcc _gdb
+
+wget https://download-mirror.savannah.gnu.org/releases/coreutils/windows-64bit-unsupported/coreutils-8.31-28-windows-64bit.zip -P /tmp
+unzip -j /tmp/coreutils-8.31-28-windows-64bit.zip -d /root/.wine/drive_c/windows/
 
 ../build.sh
